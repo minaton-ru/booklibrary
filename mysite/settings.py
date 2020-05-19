bae04ce508b1b010dc8d14c1d403dabf87d5e4ab
minaton <minaton@yandex.ru>
2020-05-19 23:52:21 +0300

    json

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')86cg#zfls8m(!wbz_nsh2u7bn-c65xxn$#@=wbk-7k16np*jf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
