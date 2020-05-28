dac72f71464e55504bec54ec2a94df6a6446e614
minaton <minaton@yandex.ru>
2020-05-28 22:33:29 +0300

    добавлен readme, поправлена верстка

--- p_library/urls.py
+++ p_library/urls.py
@@ -5,8 +5,8 @@ from .views import AuthorEdit, AuthorList, author_create_many, books_authors_cre
 app_name = 'p_library'  
 
 urlpatterns =([
-    path('author/create/', AuthorEdit.as_view(), name='author_create'),
+    path('create_author/', AuthorEdit.as_view(), name='author_create'),
     path('authors/', AuthorList.as_view(), name='author_list'),
-    path('author/create_many/', author_create_many, name='author_create_many'),
-    path('author_book/create_many/', books_authors_create_many, name='author_book_create_many'),
+    path('create_many_authors/', author_create_many, name='author_create_many'),
+    path('create_many_books_authors/', books_authors_create_many, name='author_book_create_many'),
 ], 'p_library')
